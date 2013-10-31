cdbc121a6829a8ba51b8b53b1d4f754765c27368
jdfraser <joshua.david.fraser@gmail.com>
2013-10-31 14:35:57 -0700

    added code to solarsystem to prevent planet clustering

--- game1/solarsystem.py
+++ game1/solarsystem.py
@@ -11,14 +11,26 @@ class SolarSystem(object):
 		self.y = 0
 		starImage = resources.loadImage("sun.png", center=True) 
 		self.star = physicalobject.Planet(x= self.x, y=self.y, img=starImage, batch=self.batch)
-		self.planets = range(random.randint(1, 15))
-		for newPlanet in self.planets:
+		self.planets = []
+		for i in range(random.randint(1, 15)):
+			newX, newY = self.testPosition()
 			planetImage = resources.loadImage("planet.png", center=True)
 			newPlanet = physicalobject.Planet(x=self.star.x, y=self.star.y, img=planetImage, batch=self.batch)
-			randX = random.randint(500, 1000)*random.choice([1, -1])
-			randY = random.randint(500, 1000)*random.choice([1, -1])
-			newPlanet.x = self.star.x + randX
-			newPlanet.y = self.star.y + randY
-			
+			newPlanet.x = self.star.x + newX
+			newPlanet.y = self.star.y + newY
+			self.planets.append(newPlanet)
 	def draw(self):
 		self.batch.draw()
+	
+	def testPosition(self):
+		while True:			
+			randX = random.randint(500, 1000)*random.choice([1, -1])
+			randY = random.randint(500, 1000)*random.choice([1, -1])			
+			if self.farEnough(randX, randY):
+				return randX, randY	
+	
+	def farEnough(self, x, y):
+		for planet in self.planets:
+			if Vector(x, y).distance((planet.x, planet.y)) < 300:
+				return False
+		return True
